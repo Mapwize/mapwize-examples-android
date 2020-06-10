ee1dd73e36083f29c92dafad7804298d5a548476
Etienne <etienne@mapwize.io>
2020-06-10 13:33:09 +0200

    Simple map example

---
+++ app/src/main/java/io/mapwize/mapwizeexamples/SimpleMapActivity.java
@@ -0,0 +1,111 @@
+package io.mapwize.mapwizeexamples;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.mapbox.mapboxsdk.Mapbox;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import io.mapwize.mapwizesdk.core.MapwizeConfiguration;
+import io.mapwize.mapwizesdk.map.ClickEvent;
+import io.mapwize.mapwizesdk.map.MapOptions;
+import io.mapwize.mapwizesdk.map.MapwizeView;
+
+public class SimpleMapActivity extends AppCompatActivity {
+
+    static final String MAPBOX_API_KEY = "pk.mapwize";
+    static final String MAPWIZE_API_KEY = "YOUR_API_KEY";
+    static final String MAPWIZE_VENUE_ID = "YOUR_VENUE_ID";
+
+    MapwizeView mapwizeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_map);
+        Mapbox.getInstance(this, MAPBOX_API_KEY);
+        FrameLayout container = findViewById(R.id.container);
+        MapwizeConfiguration conf = new MapwizeConfiguration.Builder(this,
+                MAPWIZE_API_KEY)
+                .build();
+
+        MapOptions options = new MapOptions.Builder()
+                .centerOnVenue(MAPWIZE_VENUE_ID)
+                .build();
+        mapwizeView = new MapwizeView(getApplicationContext(), conf, options);
+
+        mapwizeView.setLayoutParams(new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT
+        ));
+        container.addView(mapwizeView);
+        mapwizeView.onCreate(savedInstanceState);
+        mapwizeView.getMapAsync((mapwizeMap) -> {
+
+            // Mapbox and Mapwize are fully loaded.
+            mapwizeMap.addOnClickListener(event -> {
+                if (event.getEventType() == ClickEvent.VENUE_CLICK) {
+                    mapwizeMap.centerOnVenue(event.getVenuePreview(), 200);
+                }
+                else if (event.getEventType() == ClickEvent.PLACE_CLICK) {
+                    mapwizeMap.addMarker(event.getPlacePreview().getDefaultCenter());
+                    mapwizeMap.addPromotedPlace(event.getPlacePreview());
+                }
+                else {
+                    mapwizeMap.removePromotedPlaces();
+                    mapwizeMap.removeMarkers();
+                }
+            });
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapwizeView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapwizeView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mapwizeView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        mapwizeView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        mapwizeView.onSaveInstanceState(saveInstanceState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapwizeView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        mapwizeView.onDestroy();
+        super.onDestroy();
+    }
+}
