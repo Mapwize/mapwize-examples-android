5a589daf947b3091d11558d98db74eb4a3326fe9
GoJrM <femczuk.jeremy@gmail.com>
2020-09-21 13:34:25 +0200

    WIP MapOptionInitWithDefaultVenueUniverseActivity

---
+++ app/src/main/java/io/mapwize/mapwizeexamples/MapOptionInitWithDefaultVenueUniverse.java
@@ -0,0 +1,103 @@
+package io.mapwize.mapwizeexamples;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.mapbox.mapboxsdk.Mapbox;
+
+import io.mapwize.mapwizesdk.core.MapwizeConfiguration;
+import io.mapwize.mapwizesdk.map.MapOptions;
+import io.mapwize.mapwizesdk.map.MapwizeView;
+
+/**
+ * This activity uses SimpleMapActivity as starting point.
+ * The goal here is to demonstrate how to initialize your map in a specific Venue Universe
+ * You can can check the MapOptionCenteredOnVenue first if you want to know more about centeredOnVenue option usage
+ */
+
+public class MapOptionInitWithDefaultVenueUniverse extends AppCompatActivity {
+
+    static final String MAPBOX_API_KEY = "pk.mapwize";
+    static final String MAPWIZE_API_KEY = "YOUR_MAPWIZE_API_KEY";
+    static final String MAPWIZE_VENUE_ID = "56c2ea3402275a0b00fb00ac";
+    static final String MAPWIZE_UNIVERSE_ID = "58eb79f01cb35c00105f4a9e";
+
+    MapwizeView mapwizeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_option_init_with_default_venue_universe);
+        Mapbox.getInstance(this, MAPBOX_API_KEY);
+        FrameLayout container = findViewById(R.id.container);
+        MapwizeConfiguration conf = new MapwizeConfiguration.Builder(this,
+                MAPWIZE_API_KEY)
+                .build();
+
+        MapOptions options = new MapOptions.Builder()
+                .centerOnVenue(MAPWIZE_VENUE_ID)
+                .universe(MAPWIZE_UNIVERSE_ID)
+                .build();
+
+        mapwizeView = new MapwizeView(getApplicationContext(), conf, options);
+
+        mapwizeView.setLayoutParams(new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT
+        ));
+        container.addView(mapwizeView);
+        mapwizeView.onCreate(savedInstanceState);
+        mapwizeView.getMapAsync((mapwizeMap) -> {
+
+            // Mapbox and Mapwize are fully loaded.
+            /*
+                You can now do some stuff like adding places, markers, or anything else
+            */
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapwizeView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapwizeView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mapwizeView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        mapwizeView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        mapwizeView.onSaveInstanceState(saveInstanceState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapwizeView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        mapwizeView.onDestroy();
+        super.onDestroy();
+    }
+}
