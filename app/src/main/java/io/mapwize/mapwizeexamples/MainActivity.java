33df58a075366f96b4741ad71b99d664e41dae65
GoJrM <femczuk.jeremy@gmail.com>
2020-09-21 14:18:55 +0200

    WIP MapOptionSetDefaultLanguageActivity

--- app/src/main/java/io/mapwize/mapwizeexamples/MainActivity.java
+++ app/src/main/java/io/mapwize/mapwizeexamples/MainActivity.java
@@ -38,4 +38,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ShowDirectionActivity.class);
         startActivity(intent);
     }
+
+    public void loadMapWithRestrictContent(View v) {
+        Intent intent = new Intent(this, MapOptionRestrictToVenueActivity.class);
+        startActivity(intent);
+    }
 }
